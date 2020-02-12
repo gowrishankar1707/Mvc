db4e9b84beaf91f42d485eb0c56b17277ee7b894
Gower <Gower@GOWRI>
2020-02-12 18:41:48 +0530

    Add project files.

---
+++ MVCSample/Controllers/AspireController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSample.Controllers
+{
+    public class AspireController : Controller
+    {
+        // GET: Aspire
+        public ActionResult AspireIndex()
+        {
+            return View();
+        }
+    }
+}
