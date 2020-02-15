d4fae0dab70e3d28256f573917734e92c60569f9
Gower <gowrishankar17022000@gmail.com>
2020-02-15 06:29:36 +0530

    MVC Crud Operation

--- MVCSample/Controllers/LibraryUserController.cs
+++ MVCSample/Controllers/LibraryUserController.cs
@@ -11,17 +11,55 @@ namespace MVCSample.Controllers
     public class LibraryUserController : Controller
     {
         // GET: LibraryUser
+        LibraryRepositary repositary;
+        
+        public LibraryUserController()
+        {
+            repositary = new LibraryRepositary();
+        }
+        
         public ActionResult LibraryUser()
         {
             IEnumerable<User> user = LibraryRepositary.GetUser();
             ViewBag.users = user;
             ViewData["user"] = user;
             TempData["user"] = user;
-            return RedirectToAction("LibraryTemp");
+            return View();
         }
         public ActionResult LibraryTemp()
         {
+            ViewBag.user = TempData["user"];
             return View();
         }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            repositary.AddUser(user);
+            TempData["Message"] = "User Added";
+            return RedirectToAction("LibraryUser");
+        }
+        public ActionResult Edit(string userName)
+        {
+            User user = repositary.GetUserByUserName(userName);
+            return View(user);
+        }
+        public ActionResult Update(User user)
+        {
+            repositary.UpdateUser(user);
+            TempData["Message"] = "Package Updated";
+            return RedirectToAction("LibraryUser");
+        }
+        public ActionResult Delete(string userName)
+        {
+            repositary.DeleteUser(userName);
+            return RedirectToAction("LibraryUser");
+        }
     }
+
+
 }
--- UserRepositary/LibraryRepositary.cs
+++ UserRepositary/LibraryRepositary.cs
@@ -12,13 +12,32 @@ namespace UserRepositary
         public static List<User> user = new List<User>();
         static LibraryRepositary()
         {
-            user.Add(new User { name = "gowri", userName = "shankar" });
+            user.Add(new User { name = "gowri", userName = "shankar",email="gowrishankar17022000@gmail.com" });
         }
         public static IEnumerable<User> GetUser()
         {
             return user;
         }
-
+        public void AddUser(User addUser)
+        {
+            user.Add(addUser);
+        }
+        public User GetUserByUserName(string userName)
+        {
+          return  user.Find(username => username.userName == userName);
+        }
+        public void UpdateUser(User user)
+        {
+            User userentity = GetUserByUserName(user.userName);
+            userentity.userName = user.userName;
+            userentity.name = user.name;
+            userentity.email = user.email;
+        }
+        public void DeleteUser(string userName)
+        {
+            User userEntity = GetUserByUserName(userName);
+                user.Remove(userEntity);
+        }
 
     }
 }
